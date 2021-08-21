c9f523a886aa2ffa01220adf21d5f441a5a116bf
dae54 <danielernest1.05@gmail.com>
2021-08-22 01:26:36 +0300

    color palletes

---
+++ src/components/utilities/Colors.js
@@ -0,0 +1,18 @@
+const colors = {
+    primaryDark: '#293241',
+    primaryLight: '#3d5a80',
+    secondaryDark: '#98c1d9',
+    secondartLight: '#e0fbfc',
+    tertiary: '#ee6c4d',
+
+    primary: '#0d6efd',
+    secondary: '#6c757d',
+    info: '#0dcaf0',
+    dark: '#212529',
+    light: '#f8f9fa',
+    success: '#198754',
+    warning: '#ffc107',
+    danger: '#dc3545'
+}
+
+export default colors
