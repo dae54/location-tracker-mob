4ec108dc10cdce6d0549fd098bac8d0365b933d8
dae54 <danielernest1.05@gmail.com>
2021-08-24 12:31:25 +0300

    feat: all qns component

---
+++ src/screens/Assistance/AllQuestions/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AllQuestions() {
+    return (
+        <View>
+            <Text>All qyestions</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
