507056510b81201970cd57c5a2f0818f9a0a2dae
dae54 <danielernest1.05@gmail.com>
2021-08-24 16:34:23 +0300

    tabbar toggler

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { AuthProvider } from './src/context/AuthContext';
+import { TabBarVisibilityProvider } from './src/context/NavigationContext';
 
 import Router from './src/routes'
 
 export default function App() {
   return (
     <AuthProvider>
-      <Router />
+      <TabBarVisibilityProvider>
+        <Router />
+      </TabBarVisibilityProvider>
     </AuthProvider>
   );
 }
