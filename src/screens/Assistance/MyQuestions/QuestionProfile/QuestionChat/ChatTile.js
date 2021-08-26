94e8631f5146d68ed69d78c397ef139bf5c8f9f3
dae54 <danielernest1.05@gmail.com>
2021-08-26 15:45:43 +0300

    integrated chat tile

--- src/screens/Assistance/MyQuestions/QuestionProfile/QuestionChat/ChatTile.js
+++ src/screens/Assistance/MyQuestions/QuestionProfile/QuestionChat/ChatTile.js
@@ -1,15 +1,30 @@
+import moment from 'moment'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { useAuth } from '../../../../../context/AuthContext'
-
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import colors from '../../../../../components/utilities/Colors'
 export default function ChatTile({ chat }) {
     const { authData } = useAuth()
-    const toAlignRight = chat.fromId === authData._id ? true : false
+    const toAlignRight = chat.sender === authData._id ? true : false
+
 
     return (
         <View>
             <View style={[styles.card, toAlignRight ? styles.alignRight : styles.alignLeft]}>
-                <Text>lorem ipsum dolor meskques fedora lorem ipsum dolor lorem ipsum dolor meskques fedora meskques fedora</Text>
+                {/* <Text>lorem ipsum dolor meskques fedora lorem ipsum dolor lorem ipsum dolor meskques fedora meskques fedora</Text> */}
+                <Text>{chat.body}</Text>
+                <View style={{ flexDirection: 'row', alignSelf: 'flex-end' }}>
+                    <Text style={{ fontSize: 10, color: '#C0C0C0', }}>{moment(chat.createdAt).fromNow()}</Text>
+                    <Text style={{ fontSize: 10, color: '#C0C0C0', }}>
+                        {chat.delivered ?
+                            <Ionicons name='checkmark' color={colors.info} size={14} />
+                            :
+                            <Ionicons name='timer-outline' color='red' size={14} />
+                        }
+                    </Text>
+                </View>
+
             </View>
         </View>
     )
