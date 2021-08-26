6b445af9bdb68fc258f94a649040b42c4b50308a
dae54 <danielernest1.05@gmail.com>
2021-08-26 15:41:21 +0300

    cleanup

--- src/context/QuestionsContext.js
+++ src/context/QuestionsContext.js
@@ -66,7 +66,7 @@ function useQuestions() {
     const context = useContext(QuestionsContext);
 
     if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
+        throw new Error('useQuestions must be used within a QuestionsProvider');
     }
 
     return context;
