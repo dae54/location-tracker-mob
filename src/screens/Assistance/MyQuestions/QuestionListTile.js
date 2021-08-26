4c4cd9f869848521f4792be1fa4a3c9f7340d208
dae54 <danielernest1.05@gmail.com>
2021-08-26 12:34:39 +0300

    intergrate with api

--- src/screens/Assistance/MyQuestions/QuestionListTile.js
+++ src/screens/Assistance/MyQuestions/QuestionListTile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, TouchableNativeFeedback, TouchableOpacity } from 'react-native'
-
+import moment from 'moment'
 
 export default function QuestionListTile({ question, navigation }) {
     return (
@@ -12,24 +12,22 @@ export default function QuestionListTile({ question, navigation }) {
             onPress={() => {
                 navigation.navigate('question_profile', { question })
             }} >
-            {/* <View > */}
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                 <Text numberOfLines={2} style={{ width: '90%', fontWeight: 'bold', fontSize: 16, textTransform: 'capitalize' }}>
-                    lorem ipsum dolor myqqest lorem ipsum lorem ipsum dolor myqqest lorem ipsum dolor myqqest dolor myqqest
+                    {/* lorem ipsum dolor myqqest lorem ipsum lorem ipsum dolor myqqest lorem ipsum dolor myqqest dolor myqqest */}
+                    {question.title}
                 </Text>
                 <Text style={{ backgroundColor: 'green', width: 15, height: 15, borderRadius: 7.5 }} />
             </View>
             <View style={{ marginTop: 5 }}>
                 <Text numberOfLines={1} style={{ color: '#C0C0C0' }}>
-                    lorem ipsum dolor myqqest lorem ipsum lorem ipsum dolor myqqest lorem ipsum dolor myqqest dolor myqqest
-                    lorem ipsum dolor myqqest lorem ipsum lorem ipsum dolor myqqest lorem ipsum dolor myqqest dolor myqqest
+                    {question.body}
                 </Text>
             </View>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                 <Text style={{}}>Assistance: Dr. Maziku</Text>
-                <Text style={{}}>12/10/2020 1230hrs</Text>
+                <Text style={{}}>{moment(question.createdAt).format('DD MMM YYYY, HH:mm ')}</Text>
             </View>
-            {/* </View> */}
         </TouchableOpacity>
     )
 }
