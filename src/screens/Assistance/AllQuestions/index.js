34860fcff9c8dbf322fd8bf5c355b573c65aa297
dae54 <danielernest1.05@gmail.com>
2021-08-27 04:45:19 +0300

    cleanup

--- src/screens/Assistance/AllQuestions/index.js
+++ src/screens/Assistance/AllQuestions/index.js
@@ -1,12 +1,86 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect, useState } from 'react'
+import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native'
+import QuestionListTile from '../MyQuestions/QuestionListTile'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import colors from '../../../components/utilities/Colors'
+import * as QuestionsAPI from '../../../api/questionAPI'
 
-export default function AllQuestions() {
+// import { Picker } from '@react-native-picker/picker'
+
+export default function AllQuestions({ navigation }) {
+    const [filter, setFilter] = useState('')
+    const [questions, setQuestions] = useState({ loading: false, data: [] })
+
+
+    function fetchAllQuestions() {
+        QuestionsAPI.getAllQuestions()
+            .then(response => {
+                console.log(response)
+                setQuestions({ loading: false, data: response })
+            }).catch(error => {
+                console.log(error)
+            })
+    }
+
+    useLayoutEffect(() => {
+        fetchAllQuestions()
+        return () => {
+            // cleanup
+        };
+    }, [])
+
     return (
-        <View>
-            <Text>All qyestions</Text>
-        </View>
+        <View style={styles.container}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 18, marginBottom: 5 }}>
+                <View style={styles.searchProduct}>
+                    <Ionicons name='search' style={styles.searchIcon} color='#C0C0C0' size={20} />
+                    <TextInput returnKeyType='search' style={styles.subSectionInput} placeholderTextColor='#C0C0C0' placeholder='Search questions' />
+                </View>
+                {/* <Text>OR</Text> */}
+                {/* <View style={{ flex: 1, alignSelf: 'center', }}>
+                    <Picker
+                        selectedValue={filter}
+                        onValueChange={
+                            (value) => setFilter(value)
+                        }>
+                        <Picker.Item enabled={false} label="Select your filter" value={0} />
+                        <Picker.Item label="Pending" value='1' />
+                        <Picker.Item label="Answered" value='2' />
+                    </Picker>
+                </View> */}
+            </View>
+            <View style={{ marginTop: 10, flex: 1 }}>
+                < FlatList
+                    data={questions.data}
+                    renderItem={({ item: question }) => <QuestionListTile question={question} navigation={navigation} key={question._id} />}
+                    keyExtractor={(item) => item._id}
+                />
+            </View>
+        </View >
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.light,
+        paddingHorizontal: 5,
+    },
+    searchProduct: {
+        flex: 1,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 30,
+        // contents
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    searchIcon: {
+        alignSelf: 'center',
+        paddingLeft: 10
+    },
+    subSectionInput: {
+        padding: 5,
+        color: colors.primaryDark
+    },
+})
--- src/screens/Assistance/index.js
+++ src/screens/Assistance/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react';
+// import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+// import HomeScreen from "../screens/index";
+// import ProfileScreen from "../screens/profile";
+// import SettingScreen from "../screens/settings";
 
-export default function DiscoverScreen() {
-    return (
-        <View>
-            <Text>Assistance</Text>
-        </View>
-    )
-}
-
-const styles = StyleSheet.create({})
+// const AssistanceNavigator = createMaterialTopTabNavigator(
+//     {
+//         Home: HomeScreen,
+//         Profile: ProfileScreen,
+//         Settings: SettingScreen,
+//     },
+//     {
+//         tabBarOptions: {
+//             activeTintColor: 'white',
+//             showIcon: true,
+//             showLabel: false,
+//             style: {
+//                 backgroundColor: 'red'
+//             }
+//         },
+//     }
+// )
+// // export default AssistanceNavigator;
+// // export default createAppContainer(AppNavigator);
