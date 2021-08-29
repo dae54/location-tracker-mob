40ac69cdcbdebbfb4b6bd36a9894d80fb447774e
dae54 <danielernest1.05@gmail.com>
2021-08-29 18:36:09 +0300

    add raduis picker

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -7,24 +7,28 @@ export default function Settings() {
     const { mapRadius, saveSettings } = useSettings()
 
     return (
-        <View style={{ flex: 1, backgroundColor: 'white', margin: 5 }}>
+        <View style={{ flex: 1, backgroundColor: 'white', }}>
             <View style={{ width: '100%', marginBottom: 10, height: 50, backgroundColor: 'white', elevation: 1, paddingHorizontal: 10, justifyContent: 'center' }}>
                 <Text style={{ fontSize: 18 }}>Settings</Text>
             </View>
-            <View style={{ elevation: 2, backgroundColor: 'white', padding: 10, marginVertical: 5, justifyContent: 'space-between', flexDirection: 'row' }}>
-                <View style={{ width: '65%' }}>
-                    <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Assistance Covege</Text>
-                    <Text style={{ color: '#c0c0c0', fontSize: 12 }}>Change the minimum radius on map from your location so as to give priority to nearby volunteers</Text>
-                </View>
-                <View style={{ width: '35%' }}>
-                    <Picker
-                        style={{ elevation: 1, backgroundColor: 'white', textAlign: 'center', }}
-                        selectedValue={mapRadius}
-                        onValueChange={(value) => saveSettings({ key: '@mapRadius', value: JSON.stringify(value) })}>
-                        <Picker.Item label="1 km" value={1000} />
-                        <Picker.Item label="10 km" value={10000} />
-                        <Picker.Item label="100 km" value={100000} />
-                    </Picker>
+            <View style={{ marginHorizontal: 5 }}>
+                <View style={{ elevation: 2, backgroundColor: 'white', padding: 10, marginVertical: 5, justifyContent: 'space-between', flexDirection: 'row' }}>
+                    <View style={{ width: '65%' }}>
+                        <Text style={{ fontWeight: 'bold', fontSize: 16 }}>Assistance Covege</Text>
+                        <Text style={{ color: '#c0c0c0', fontSize: 12 }}>Change the maximum radius on map from your location so as to give priority to nearby volunteers</Text>
+                    </View>
+                    <View style={{ width: '35%' }}>
+                        <Picker
+                            style={{ elevation: 1, backgroundColor: 'white', textAlign: 'center', }}
+                            selectedValue={mapRadius}
+                            onValueChange={(value) => saveSettings({ key: '@mapRadius', value: JSON.stringify(value) })}>
+                            <Picker.Item label="10 km" value={10000} />
+                            <Picker.Item label="50 km" value={50000} />
+                            <Picker.Item label="100 km" value={100000} />
+                            <Picker.Item label="500 km" value={500000} />
+                            <Picker.Item label="1,000 km" value={1000000} />
+                        </Picker>
+                    </View>
                 </View>
             </View>
         </View>
