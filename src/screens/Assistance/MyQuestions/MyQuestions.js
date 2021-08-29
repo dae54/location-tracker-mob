cc6d0f976d40d884131d48f0628e47939cf92499
dae54 <danielernest1.05@gmail.com>
2021-08-29 18:35:15 +0300

    add fallback ui

--- src/screens/Assistance/MyQuestions/MyQuestions.js
+++ src/screens/Assistance/MyQuestions/MyQuestions.js
@@ -1,15 +1,19 @@
 import React, { useLayoutEffect, useState } from 'react'
-import { FlatList, StyleSheet, Text, TextInput, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, FlatList, RefreshControl, StyleSheet, Text, TextInput, TouchableNativeFeedback, View } from 'react-native'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import colors from '../../../components/utilities/Colors'
 import { useQuestions } from '../../../context/QuestionsContext'
 import NewQuestion from './NewQuestion'
 import QuestionListTile from './QuestionListTile'
+import Empty from '../../../components/utilities/Empty'
+import FeedbackHandler from '../../../components/utilities/FeedbackHandler'
+
 
 export default function MyQuestions({ navigation }) {
     // const [questions, setQuestions] = useState({ loading: false, data: Array.from('daniel amani') })
-    const { userQuestions, getUserQuestions } = useQuestions()
+    const { userQuestions, getUserQuestions, loading } = useQuestions()
+    const [refreshing, setRefreshing] = React.useState(false);
 
     useLayoutEffect(() => {
         getUserQuestions()
@@ -22,7 +26,7 @@ export default function MyQuestions({ navigation }) {
             <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 18, marginBottom: 5 }}>
                 <View style={{ flex: 1, alignSelf: 'center', }}>
                     <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Questions Asked</Text>
-                    <Text style={{ fontSize: 14, color: '#C0C0C0' }}>300</Text>
+                    <Text style={{ fontSize: 14, color: '#C0C0C0' }}>{userQuestions.data.length}</Text>
                 </View>
                 <View style={styles.searchProduct}>
                     <Ionicons name='search' style={styles.searchIcon} color='#C0C0C0' size={20} />
@@ -30,11 +34,51 @@ export default function MyQuestions({ navigation }) {
                 </View>
             </View>
             <View style={{ marginTop: 10, flex: 1 }}>
-                < FlatList
-                    data={userQuestions}
-                    renderItem={({ item: question }) => <QuestionListTile question={question} navigation={navigation} key={question._id} />}
-                    keyExtractor={(item) => item._id}
-                />
+                {loading ?
+                    <View>
+                        <ActivityIndicator color='#ccc' animating size={30} />
+                        <Text style={{ textAlign: 'center' }}>Plese wait...</Text>
+                    </View>
+                    :
+                    userQuestions.data.length === 0 ?
+                        <View>
+                            <Empty />
+                            {
+                                userQuestions.error ?
+                                    <View>
+                                        <FeedbackHandler message={{ status: 'error', data: userQuestions.error }} />
+                                        <TouchableNativeFeedback onPress={getUserQuestions}>
+                                            <View style={{ width: 50, alignSelf: 'center', padding: 10, borderRadius: 5 }}>
+                                                <Ionicons name='refresh' size={30} style={{ alignSelf: 'center' }} />
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    </View>
+                                    :
+                                    <View>
+                                        <Text style={{ textAlign: 'center', fontSize: 16, width: '50%', alignSelf: 'center', marginTop: 30, textTransform: 'capitalize' }}>
+                                            You dont have any questions. Added questions will be listed here
+                                        </Text>
+                                        <TouchableNativeFeedback onPress={getUserQuestions}>
+                                            <View style={{ width: 50, alignSelf: 'center', padding: 10, borderRadius: 5 }}>
+                                                <Ionicons name='refresh' size={30} style={{ alignSelf: 'center' }} />
+                                            </View>
+                                        </TouchableNativeFeedback>
+                                    </View>
+                            }
+                        </View>
+                        :
+                        < FlatList
+                            refreshControl={
+                                <RefreshControl
+                                    refreshing={refreshing}
+                                    onRefresh={getUserQuestions}
+                                />
+                            }
+                            data={userQuestions.data}
+                            renderItem={({ item: question }) => <QuestionListTile question={question} navigation={navigation} key={question._id} />}
+                            keyExtractor={(item) => item._id}
+                        />
+                }
             </View>
             <TouchableNativeFeedback activeOpacity={0.8}
                 onPress={() => setModalVisible(true)}>
