6f75d54594b2cd3db65f9fc3496e38ad167df587
dae54 <danielernest1.05@gmail.com>
2021-08-31 12:36:27 +0300

    remove unwanted codes

--- src/components/reusables/OverviewTile.js
+++ src/components/reusables/OverviewTile.js
@@ -1,27 +1,13 @@
 import React from 'react'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 
-import { StyleSheet, Text, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native'
-
-function OverviewTile({ title, icon, value, link, currency }) {
+function OverviewTile({ title, value, link, currency }) {
     return (
-        <TouchableNativeFeedback background={TouchableNativeFeedback.Ripple('whitesmoke')} >
-            <View style={styles.overviewTile}>
-                <Ionicons
-                    style={styles.overviewIcon}
-                    name={icon}
-                    size={20}
-                    color='#C0C0C0'
-                />
-                <View>
-                    <Text style={styles.overviewValue}>{value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {currency && 'Tsh'}</Text>
-                    <Text style={styles.overviewTitle}>{title}</Text>
-                </View>
-                <View>
-                    <Text style={{ color: '#C0C0C0' }}>Tap to view</Text>
-                </View>
-            </View>
-        </TouchableNativeFeedback>
+        <TouchableOpacity activeOpacity={0.9} style={styles.overviewTile}>
+            <Text style={styles.overviewTitle}>{title}</Text>
+            <Text style={styles.overviewValue}>{value} {currency && 'Tsh'}</Text>
+            {/* <Text style={styles.overviewValue}>{value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {currency && 'Tsh'}</Text> */}
+        </TouchableOpacity>
     )
 }
 
@@ -30,26 +16,13 @@ export default OverviewTile
 const styles = StyleSheet.create({
     overviewTile: {
         backgroundColor: 'white',
-        height: '25%',
-        flexGrow: 1,
-        margin: '3%',
-        elevation: 2,
-        paddingVertical: 12,
+        justifyContent: 'space-between',
+        margin: 5,
+        elevation: 1,
+        paddingVertical: 25,
         paddingHorizontal: 20,
         borderRadius: 15,
-        // contents
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between'
-    },
-    overviewIcon: {
-        borderWidth: 1,
-        height: 40,
-        width: 40,
-        borderColor: '#C0C0C0',
-        borderRadius: 20,
-        padding: 10,
-        backgroundColor: 'white'
+        width: '50%'
     },
     overviewValue: {
         fontSize: 26,
